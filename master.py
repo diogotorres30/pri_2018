c520de7d05f4deb565c3721954b74b68e46e1539
unknown <diogo.torres.30@gmail.com>
2018-12-04 16:09:13 +0000

    a) e c) feitos e parcialmente comentados

--- master.py
+++ master.py
@@ -11,47 +11,54 @@ import whoosh.lang
 import nltk
 from nltk.probability import FreqDist
 import spacy
-nlp = spacy.load('en_core_web_sm')
-nlp.max_length = 10000000
 import io
 import glob
-import re
+nlp = spacy.load('en_core_web_sm') #Loading english language for Named entity recognition with spaCy
+nlp.max_length = 10000000 #Increasing maximum file size to be processed
+
+#party_names and party_manifestos were created for easier and faster manipulation of the provided data
 party_names = ["Conservative Party", "Democratic Unionist Party", "Green Party of England and Wales", "Labour Party", "Liberal Democrats", "Scottish National Party", "Social Democratic and Labour Party", "The Party of Wales", "Ulster Unionist Party", "United Kingdom Independence Party", "We Ourselves"]
 party_manifestos = dict()
 name_counter = 0
+party_manifesto_entities = []
+all_parties_manifestos_entities = []
 
+#Loading files containing each party's complete manifestos organized line by line
 for filepath in glob.iglob('helpers/training_tasks/*.txt'):	
 	party_manifestos[party_names[name_counter]] = io.open(filepath, 'r+').read()
 	name_counter = name_counter + 1 
 name_counter = 0
-for p in party_manifestos.keys():
-	print(p)
-# schema = Schema(manifesto_id = TEXT(stored=True), content=TEXT(stored=True, analyzer=analysis.StandardAnalyzer(stoplist=None)), party = TEXT(stored=True), date = TEXT(stored=True), title = TEXT(stored=True))
-# ix = create_in("indexdir", schema)
-# writer = ix.writer()
-text_in_list_form = []
-big_boy = []
-firstline = True
+
+#Populating index, from original csv file, to be used by Whoosh Search Engine
+schema = Schema(manifesto_id = TEXT(stored=True), content=TEXT(stored=True, analyzer=analysis.StandardAnalyzer(stoplist=None)), party = TEXT(stored=True), date = TEXT(stored=True), title = TEXT(stored=True))
+ix = create_in("indexdir", schema)
+writer = ix.writer()
+
 csv.field_size_limit(1000000)
 csvfile = open('en_docs_clean.csv', 'r',encoding="utf8")
 spamreader = csv.reader(csvfile, quotechar='"', delimiter=',',quoting=csv.QUOTE_ALL, skipinitialspace=True)
-# 	writer.add_document(manifesto_id=row[1],content=row[0],party=row[2],date=row[3],title=row[4])
-# writer.commit()
+
+for row in spamreader:
+	writer.add_document(manifesto_id=row[1],content=row[0],party=row[2],date=row[3],title=row[4])
+writer.commit()
 
 print('****************************************************************************')
 print('****************************************************************************')
 print('****************************************************************************')
-for p in party_manifestos.keys():
-	print("The most mentioned entities by the " + p + " are:")
-	doc = nlp(" ".join(party_manifestos.get(p).split()))
-	for t in doc.ents:
-		text_in_list_form.append(t.text)
-		big_boy.append(t.text)
-	print(FreqDist(text_in_list_form).most_common(5))
-	print()
-	text_in_list_form.clear()
-print("The most mentioned entities by all are:")
-print(FreqDist(big_boy).most_common(5))
+#Findind each party's entities and declraring which are the most commonly used by each one and in total.
+#We chose spaCy for its ease of use
+
+# for p in party_manifestos.keys():
+# 	print("The most mentioned entities by the " + p + " are:")
+# 	doc = nlp(" ".join(party_manifestos.get(p).split()))
+# 	for t in doc.ents:
+# 		party_manifesto_entities.append(t.text)
+# 		all_parties_manifestos_entities.append(t.text)
+# 	print(FreqDist(party_manifesto_entities).most_common(5))
+# 	print()
+# 	party_manifesto_entities.clear()
+# print("The most mentioned entities by all are:")
+# print(FreqDist(all_parties_manifestos_entities).most_common(5))
 print('****************************************************************************')
 print('****************************************************************************')
 print('****************************************************************************')
@@ -62,7 +69,8 @@ print('*************************************************************************
 user_query = sys.argv[1]
 ix = open_dir("indexdir")
 parties = dict()
-mentioned_by_others = [0,0,0,0,0,0,0,0,0,0,0,0]
+mentioned_by_others = [0,0,0,0,0,0,0,0,0,0,0]
+mentions_others = [0,0,0,0,0,0,0,0,0,0,0]
 manifestos_results = []
 print()
 print()
@@ -93,14 +101,29 @@ with ix.searcher() as searcher:
 		print('Number of manifestos per party for the keyword: "', sub_query, '"')
 		print('----------------------------------------------------------------------------')
 		for p in party_manifestos.keys():
-			for ppp in party_manifestos.keys():
-				if ppp != p:
-					mentioned_by_others[name_counter] = mentioned_by_others[name_counter] + " ".join(party_manifestos.get(ppp).split()).lower().count(p.lower())
+			
 			
 			print(p, ': ', " ".join(party_manifestos.get(p).split()).lower().count(sub_query.lower()))
-			name_counter = name_counter +1
+			
 		print('****************************************************************************')
-		name_counter = 0
 		
-	print(mentioned_by_others)
+
+for p in party_manifestos.keys():
+	for ppp in party_manifestos.keys():
+		if ppp != p:
+			mentioned_by_others[name_counter] = mentioned_by_others[name_counter] + " ".join(party_manifestos.get(ppp).split()).lower().count(p.lower())
+	print("The " + p + " is mentioned by other parties", mentioned_by_others[name_counter], "times.")
+	print()
+	name_counter = name_counter + 1
+name_counter = 0
+print("iuyghisdughiudghiuhgiusfdhguiodsfhgiudsnjxczbhdsbgiasdgbiadgbiuaghiadsbghsagbishabfuhdsabfudsbusabfuybzubgbguydgbuyabguyadbg")
+for p in party_manifestos.keys():
+	for ppp in party_manifestos.keys():
+		if ppp != p:
+			mentions_others[name_counter] = mentions_others[name_counter] + " ".join(party_manifestos.get(p).split()).lower().count(ppp.lower())
+	print("The " + p + " mentions other parties", mentions_others[name_counter], "times.")
+	print()
+	name_counter = name_counter + 1
+name_counter = 0
+
 #////////////////////////////////////////  ALINEA a) /////////////////////////////////////////////
